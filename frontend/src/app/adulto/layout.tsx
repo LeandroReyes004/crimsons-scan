a8c39e7be72dbb5d2cb027471f31ce2105b74ea0
LeandroReyes004 <leandro.elias1025@gmail.com>
2026-09-01 21:17:55 -0400

    chore: remover scripts de anuncios Adcash (aclib)

--- frontend/src/app/adulto/layout.tsx
+++ frontend/src/app/adulto/layout.tsx
@@ -7,25 +7,6 @@ export default function AdultoLayout({
 }) {
   return (
     <>
-      <Script id="aclib" src="//acscdn.com/script/aclib.js" strategy="afterInteractive" />
-      <Script id="aclib-run" strategy="afterInteractive">
-        {`
-          const initAclib = setInterval(() => {
-            if (typeof aclib !== 'undefined' && typeof aclib.runAutoTag === 'function') {
-              aclib.runAutoTag({
-                  zoneId: 'i0wqo2yu4z',
-              });
-              clearInterval(initAclib);
-            } else if (typeof aclib !== 'undefined' && typeof aclib.runAutoTag !== 'function') {
-              // Si aclib existe pero runAutoTag no (p. ej. bloqueado por Brave Shields), cancelamos el intervalo.
-              clearInterval(initAclib);
-            }
-          }, 300);
-          
-          // Cleanup after 10s if it fails to load
-          setTimeout(() => clearInterval(initAclib), 10000);
-        `}
-      </Script>
       {children}
     </>
   );
